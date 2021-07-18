695bd278c74f77036f177b1d0dbbf1eacf8cfff7
volanja <volaaanja@gmail.com>
2021-07-18 23:59:24 +0900

    Update   Restrict usable command.   Add option.   Modfiy usage.

--- govc/govc-conftest.sh
+++ govc/govc-conftest.sh
@@ -13,30 +13,67 @@ function check_command () {
 }
 
 function usage () {
-    echo "A govc plugin for using Conftest to test objects in VMware vSphere using Open Policy Agent"
-    echo
-    echo "See below site for more information"
-    echo "   https://github.com/open-policy-agent/conftest"
-    echo "   https://github.com/vmware/govmomi/tree/master/govc"
-    echo
-    echo "Usage:"
-    echo "   conftest govc [command]"
-    echo
+cat <<- EOS
+A govc plugin for using Conftest to test objects in VMware vSphere using Open Policy Agent
+
+See below site for more information
+   https://github.com/open-policy-agent/conftest
+   https://github.com/vmware/govmomi/tree/master/govc
+
+Usage:
+   conftest govc [command] (NAME)
+
+Available Commands:
+  about       show vSphere informaion
+  *.info      show information
+  *.ls        show list
+
+  NOTICE:
+    High-impact operations is not support. e.g.: *.add, *.remove, *.destory and so on.
+
+OPTION:
+  NAME        specify target
+
+govc Flags:
+  flags is not supported.
+EOS
+}
+
+# e.g. govc datacenter.info
+function run_with_one_argument () {
+    echo "Testing $1"
+    if output=$(govc "$1" --json); then
+        echo "$output" | conftest test -
+    fi
+}
+
+# e.g. govc vm.info --json CentOS8
+function run_with_two_argument () {
+    echo "Testing $1 $2"
+    if output=$(govc "$1" --json $2); then
+        echo "$output" | conftest test -
+    fi
 }
 
 # Check the required commands are available on the PATH
 check_command "govc"
 check_command "conftest"
 
-if [[ ($# -eq 0) || ($1 == "--help") || ($1 == "-h") ]]; then
-    # No commands or the --help flag passed and we'll show the usage instructions
+if [[ ($# -eq 0) || ($# -gt 2 )|| ($1 == "--help") || ($1 == "-h") ]]; then
+    # No commands or many option or the --help flag passed and we'll show the usage instructions
     usage
-elif [[ ($# -eq 1) && $1 =~ ^[a-z\.]+$ ]]; then
-    # If we have one argument then we assume govc command
-    if output=$(govc "$1" --json); then
-        echo "Testing $1"
-        echo "$output" | conftest test -
-    fi
+    exit 1
+fi
+
+# *.ls , *.info
+if [[ $1 =~ ^about$ || $1 =~ ^[a-z\.]+\.ls$ || $1 =~ ^[a-z\.]+\.info$ ]]; then
+  if [[ ($# -eq 1) ]]; then
+    # If we have one argument then we assume govc command.
+    run_with_one_argument $1
+  elif [[ ($# -eq 2) ]]; then
+    # If we have two argument then we assume govc command with target.
+    run_with_two_argument $1 $2
+  fi
 else
     echo "Please check the arguments to govc conftest"
     echo
